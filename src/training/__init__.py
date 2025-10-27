cba9d391af190e39f5d7118f60ed731060889e39
Jeen-Freecs <adilkhan.bakridenov@gmail.com>
2025-10-27 06:23:56 +0000

    baseline training

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,6 +1,6 @@
 """Training utilities"""
 
-from .trainer import train_kd_model
+from .trainer import train_kd_model, create_kd_module_from_config
 
-__all__ = ["train_kd_model"]
+__all__ = ["train_kd_model", "create_kd_module_from_config"]
 
